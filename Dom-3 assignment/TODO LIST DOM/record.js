2037afe3ee3915629fd1990ea9c876ef0ed70ef2
pravin7878 <143276463+pravin7878@users.noreply.github.com>
2023-11-12 16:37:22 +0530

    todo App

--- Dom-3 assignment/TODO LIST DOM/record.js
+++ Dom-3 assignment/TODO LIST DOM/record.js
@@ -1,7 +1,28 @@
-let myform = document.getElementsByTagName('form')
-let inpTask = document.getElementById('task').innerText.value
-console.log(inpTask)
-// let priority = document.getElementById('priority').innerText.value
-// form.addEventListener('submit',function(){
-//     console.log(form.innerText)
+// let heading = document.getElementById('head')
+//   let button = document.getElementById('btn')
+//   button.addEventListener('click',function(){
+//     heading.innerText = "i am a heading"
+    // console.log(heading)
+//   })
+let myform = document.querySelector('form');
+let inptask = document.getElementById('task')
+let priority = document.getElementById('priority');
+let tbody = document.querySelector('tbody')
+let alldata = [];
+ myform.addEventListener('submit',function(e){
+    e.preventDefault();
+    let data = {};
+    data.input1 = inptask.value;
+    data.input2 = priority.value;
+    alldata.push(data);
+    tbody.innerText = null;
+    alldata.map((ele)=>{
+      let row = document.createElement('tr')
+      let td1 = document.createElement('td')
+      let td2 = document.createElement('td')
+      td1.innerText = ele.input1
+      td2.innerText = ele.input2
+      row.append(td1,td2)
+      tbody.append(row)
+    })
 })
