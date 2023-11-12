ae898065b80d805e95390f199e7583fff1fadb1f
pravin7878 <143276463+pravin7878@users.noreply.github.com>
2023-11-12 06:39:06 +0530

    form

---
+++ Dom-3 assignment/TODO LIST DOM/record.js
@@ -0,0 +1,7 @@
+let myform = document.getElementsByTagName('form')
+let inpTask = document.getElementById('task').innerText.value
+console.log(inpTask)
+// let priority = document.getElementById('priority').innerText.value
+// form.addEventListener('submit',function(){
+//     console.log(form.innerText)
+})
